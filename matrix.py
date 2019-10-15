0b6c7ed561c4bef551c39256b8bd5f8ec10fe55e
Toan Tran <toantranct94@gmail.com>
2019-10-15 21:09:04 +0700

    Toan update

--- matrix.py
+++ matrix.py
@@ -1,7 +1,7 @@
 import numpy as np 
 
 class Matrix:
-    def __init__(self, rows=None,cols=None, numbers=None):
+    def __init__(self, rows=None,cols=None, numbers=None, name=None):
         '''
         rows: số hàng 
         cols: cố cột
@@ -9,6 +9,7 @@ class Matrix:
         self.rows = rows
         self.cols = cols
         self.numbers = numbers
+        self.name = name
         self.matrix  = self._init_matrix()
         pass
 
@@ -17,6 +18,11 @@ class Matrix:
         matrix = np.asarray(self.numbers)
         # reshape to m rows x n cols
         matrix = np.reshape(matrix, (self.rows, self.cols))
+        if self.rows == 1 or self.cols == 1:
+            prefix = 'Vector'
+        else:
+            prefix = 'Matrix'
+        print("{} {}: \n".format(prefix,self.name), matrix)
         return matrix
 
     def get(self):
@@ -105,6 +111,11 @@ class MatrixCalculator:
         result = np.linalg.det(matrix)
         print("det: \n", result)
         return result
+    
+    def eigenvalues(self, matrix):
+        eigenvalues, _ = np.linalg.eig(matrix)
+        print("eigenvalues: \n", eigenvalues)
+        return eigenvalues
 
 if __name__ == "__main__":
     calculator = MatrixCalculator()
@@ -115,13 +126,16 @@ if __name__ == "__main__":
     numbers_A = [2, 0, -3, 2, -1, 8, 10, -5]
     numbers_B = [0, -4, -7, 2, 12, 3, 7, 9]
     numbers_C = [2, 0, 2, -4, 9, 5, 6, 0, -6]
-    matrix_A = Matrix(rows=2, cols=4, numbers=numbers_A).get()
-    matrix_B = Matrix(rows=2, cols=4, numbers=numbers_B).get()
-    matrix_C = Matrix(rows=3, cols=3, numbers=numbers_C).get()
+    matrix_A = Matrix(rows=2, cols=4, numbers=numbers_A, name='A').get()
+    matrix_B = Matrix(rows=2, cols=4, numbers=numbers_B, name='B').get()
+    matrix_C = Matrix(rows=3, cols=3, numbers=numbers_C, name='C').get()
 
-    AplusB = calculator.plus(matrix_A, matrix_B)
-    BsubA = calculator.subtract(matrix_B, matrix_A)
-    AplusC = calculator.plus(matrix_A, matrix_C)
+    print("A + B")
+    calculator.plus(matrix_A, matrix_B)
+    print("B - A")
+    calculator.subtract(matrix_B, matrix_A)
+    print("A + C")
+    calculator.plus(matrix_A, matrix_C)
 
     print("*"*100)
 
@@ -132,10 +146,10 @@ if __name__ == "__main__":
     numbers_A = [0, 9, 2, -3, -1, 1]
     numbers_B = [8, 1, -7, 0, 4, -1]
     numbers_C = [2, 3, -2, 5, 10, -6]
-    matrix_A = Matrix(rows=3, cols=2, numbers=numbers_A).get()
-    matrix_B = Matrix(rows=3, cols=2, numbers=numbers_B).get()
-    matrix_C = Matrix(rows=3, cols=2, numbers=numbers_C).get()
-    
+    matrix_A = Matrix(rows=3, cols=2, numbers=numbers_A, name='A').get()
+    matrix_B = Matrix(rows=3, cols=2, numbers=numbers_B, name='B').get()
+    matrix_C = Matrix(rows=3, cols=2, numbers=numbers_C, name='C').get()
+    print("3A + 2B - 1/2C")
     calculator.compute_ex_3(matrix_A, matrix_B, matrix_C)
 
     print("*"*100)
@@ -145,14 +159,15 @@ if __name__ == "__main__":
     print("VD 4 trang 37")
     numbers_A = [4, -10, 3]
     numbers_B = [-4, 3, 8]
-    matrix_A = Matrix(rows=1, cols=3, numbers=numbers_A).get()
-    matrix_B = Matrix(rows=3, cols=1, numbers=numbers_B).get()
+    matrix_A = Matrix(rows=1, cols=3, numbers=numbers_A, name='A').get()
+    matrix_B = Matrix(rows=3, cols=1, numbers=numbers_B, name='B').get()
     calculator.multiply(matrix_A, matrix_B)
     print("VD 5 trang 37")
     numbers_A = [1, -3, 0, 4, -2, 5, -8, 9]
     numbers_C = [8, 5, 3, -3, 10, 2, 2, 0, -4, -1, -7, 5]
-    matrix_A = Matrix(rows=2, cols=4, numbers=numbers_A).get()
-    matrix_C = Matrix(rows=4, cols=3, numbers=numbers_C).get()
+    matrix_A = Matrix(rows=2, cols=4, numbers=numbers_A, name='A').get()
+    matrix_C = Matrix(rows=4, cols=3, numbers=numbers_C, name='C').get()
+    print("A * C")
     calculator.multiply(matrix_A, matrix_C)
 
     print("*"*100)
@@ -161,8 +176,10 @@ if __name__ == "__main__":
     '''
     print("VD 7 trang 40")
     second_row_A = calculator.get_rows(matrix_A, 1)
+    print("A[1] * C")
     calculator.multiply(second_row_A, matrix_C)
     third_col_C = calculator.get_cols(matrix_C, 2)
+    print("A * C[2]")
     calculator.multiply(matrix_A, third_col_C)
 
     print("*"*100)
@@ -192,11 +209,11 @@ if __name__ == "__main__":
     numbers_C = [9, -1, 8]
     numbers_D = [15]
     numbers_E = [-12, -7, -7, 10]
-    matrix_A = Matrix(rows=2, cols=4, numbers=numbers_A).get()
-    matrix_B = Matrix(rows=3, cols=3, numbers=numbers_B).get()
-    matrix_C = Matrix(rows=3, cols=1, numbers=numbers_C).get()
-    matrix_D = Matrix(rows=1, cols=1, numbers=numbers_D).get()
-    matrix_E = Matrix(rows=2, cols=2, numbers=numbers_E).get()
+    matrix_A = Matrix(rows=2, cols=4, numbers=numbers_A, name='A').get()
+    matrix_B = Matrix(rows=3, cols=3, numbers=numbers_B, name='B').get()
+    matrix_C = Matrix(rows=3, cols=1, numbers=numbers_C, name='C').get()
+    matrix_D = Matrix(rows=1, cols=1, numbers=numbers_D, name='D').get()
+    matrix_E = Matrix(rows=2, cols=2, numbers=numbers_E, name='E').get()
 
     matrices = [matrix_A, matrix_B, matrix_C, matrix_D, matrix_E]
     for matrix in matrices:
@@ -210,7 +227,7 @@ if __name__ == "__main__":
     '''
     print("VD 4 trang 50")
     numbers_A = [-7, 3, 5, 1]
-    matrix_A = Matrix(rows=2, cols=2, numbers=numbers_A).get()
+    matrix_A = Matrix(rows=2, cols=2,  numbers=numbers_A, name='A').get()
     # A^2
     calculator.powm(matrix_A, 2)
     # A^3
@@ -225,7 +242,7 @@ if __name__ == "__main__":
     '''
     print("VD 3 trang 54")
     numbers_A = [-4, -2, 5, 5]
-    matrix_A = Matrix(rows=2, cols=2, numbers=numbers_A).get()
+    matrix_A = Matrix(rows=2, cols=2, numbers=numbers_A, name='A').get()
     inverse_A = calculator.inverse(matrix_A)
     calculator.powm(inverse_A, 3)
 
@@ -235,7 +252,7 @@ if __name__ == "__main__":
     '''
     print("VD 2 trang 68")
     numbers_C = [3, 1, 0, -1, 2, 2, 5, 0, -1]
-    matrix_C = Matrix(rows=3, cols=3, numbers=numbers_C).get()
+    matrix_C = Matrix(rows=3, cols=3, numbers=numbers_C, name='C').get()
     inverse_C = calculator.inverse(matrix_C)
     calculator.multiply(matrix_C, inverse_C)
     calculator.multiply(inverse_C, matrix_C)
@@ -247,8 +264,8 @@ if __name__ == "__main__":
     print("VD 1 trang 84")
     numbers_A = [3, 1, 0, -1, 2, 2, 5, 0, -1]
     numbers_B = [6, -7, 10]
-    matrix_A = Matrix(rows=3, cols=3, numbers=numbers_A).get()
-    matrix_B = Matrix(rows=3, cols=1, numbers=numbers_B).get()
+    matrix_A = Matrix(rows=3, cols=3, numbers=numbers_A, name='A').get()
+    matrix_B = Matrix(rows=3, cols=1, numbers=numbers_B, name='B').get()
 
     inverse_A = calculator.inverse(matrix_A)
     calculator.multiply(inverse_A, matrix_B)
@@ -261,9 +278,53 @@ if __name__ == "__main__":
     numbers_A = [3, 2, -9, 5]
     numbers_B = [3, 5, 4, -2, -1, 8, -11, 1, 7]
     numbers_C = [2, -6, 2, 2, -8, 3, -3, 1, 1]
-    matrix_A = Matrix(rows=2, cols=2, numbers=numbers_A).get()
-    matrix_B = Matrix(rows=3, cols=3, numbers=numbers_B).get()
-    matrix_C = Matrix(rows=3, cols=3, numbers=numbers_C).get()
+    matrix_A = Matrix(rows=2, cols=2, numbers=numbers_A, name='A').get()
+    matrix_B = Matrix(rows=3, cols=3, numbers=numbers_B, name='B').get()
+    matrix_C = Matrix(rows=3, cols=3, numbers=numbers_C, name='C').get()
     calculator.det(matrix_A)
     calculator.det(matrix_B)
     calculator.det(matrix_C)
+
+    print("*"*100)
+    '''
+    VD 1-3 trang 178
+    '''
+    print("VD 1 trang 178")
+    print("A")
+    nubmers_reflecion_xz = [1, 0, 0, 0, -1, 0, 0, 0, 1]
+    matrix_reflection_xz = Matrix(rows=3, cols=3, numbers=nubmers_reflecion_xz).get()
+    numbers_x = [2, -4, 1]
+    vector_x = Matrix(rows=3, cols=1,numbers=numbers_x).get()
+    calculator.multiply(matrix_reflection_xz, vector_x)
+    print("B")
+    nubmers_projection_x = [1, 0, 0, 0, 0, 0, 0, 0, 0]
+    matrix_projection_x = Matrix(rows=3, cols=3, numbers=nubmers_projection_x).get()
+    numbers_x = [10, 7, -9]
+    vector_x = Matrix(rows=3, cols=1,numbers=numbers_x).get()
+    calculator.multiply(matrix_projection_x, vector_x)
+    print("C")
+    nubmers_projection_yz = [0, 0, 0, 0, 1, 0, 0, 0, 1]
+    matrix_projection_yz = Matrix(rows=3, cols=3, numbers=nubmers_projection_yz).get()
+    calculator.multiply(matrix_projection_yz, vector_x)
+    print("VD 2 trang 178")
+
+
+    print("*"*100)
+    '''
+    VD 3 trang 312
+    '''
+    print("VD 3 trang 312")
+    numbers_A = [4, 0, 1, -1, -6, -2, 5, 0, 0]
+    numbers_B = [6, 3, -8, 0, -2, 0, 1, 0, -3]
+    numbers_C = [0, 1, 1, 1, 0, 1, 1, 1, 0]
+    numbers_D = [4, 0, -1, 0, 3, 0, 1, 0, 2]
+    matrix_A = Matrix(rows=3, cols=3, numbers=numbers_A, name='A').get()
+    matrix_B = Matrix(rows=3, cols=3, numbers=numbers_B, name='B').get()
+    matrix_C = Matrix(rows=3, cols=3, numbers=numbers_C, name='C').get()    
+    matrix_D = Matrix(rows=3, cols=3, numbers=numbers_D, name='D').get()
+
+    calculator.eigenvalues(matrix_A)
+    calculator.eigenvalues(matrix_B)
+    calculator.eigenvalues(matrix_C)
+    calculator.eigenvalues(matrix_D)
+    
---
+++ optimize_toan.py
@@ -0,0 +1,122 @@
+import numpy as np 
+from sympy import Symbol
+from math import e
+import matplotlib.pyplot as plt
+
+s = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+x = Symbol('x')
+
+class Optimizer:
+    def __init__(self):
+        pass
+
+    def derivative_calculator(self, function_fx):
+        if isinstance(function_fx, str):
+            function_fx = eval(function_fx)
+        derivative = function_fx.diff(x)
+        return derivative
+
+    def find_extreme(self, function_fx, a, x):
+        derivative_1 = self.derivative_calculator(function_fx)
+        print("Đạo hàm bậc nhất: \n", derivative_1)
+        print("Đạo hàm bậc nhất bằng 0 khi x = {}".format(x))
+
+        derivative_2 = self.derivative_calculator(derivative_1)
+        print("Đạo hàm bậc 2: \n", derivative_2)
+        derivative_2_value = eval(str(derivative_2))
+        print("Khi x = {}, giá trị của đạo hàm bậc 2: \n".format(x), derivative_2_value)
+
+        if derivative_2_value > 0:
+            # min
+            print("x = {} là điểm cực tiểu".format(x))
+            pass
+        else:
+            #max
+            print("x = {} là điểm cực đại".format(x))
+            pass
+
+    def draw(self, function_fx, x):
+        function_fx = str(function_fx)
+        y = eval(function_fx)
+        fig, ax = plt.subplots()
+        ax.plot(x, y)
+        ax.set_aspect('equal')
+        ax.grid(True, which='both')
+        plt.xlabel('f(x) = {}'.format(function_fx), fontsize=18)
+        ax.axhline(y=0, color='k')
+        ax.axvline(x=0, color='k')
+        plt.show()
+        pass
+
+    def draw_two(self, function_fx, function_gx, x):
+        function_fx = str(function_fx)
+        y = eval(function_fx)
+        function_gx = str(function_gx)
+        y_gx = eval(function_gx)
+        fig, ax = plt.subplots()
+
+        ax.plot(x, y, label='f(x)')
+        ax.plot(x, y_gx, label='g(x)')
+        ax.set_aspect('equal')
+        ax.grid(True, which='both')
+        ax.axhline(y=0, color='k')
+        ax.axvline(x=0, color='k')
+        ax.legend()
+        plt.show()
+        pass
+
+    def bisection_algorithm(self, function_gx):
+
+        '''
+        function_gx > 0 where x > 2
+        otherwise, function_gx < 0 where x < 2
+        '''
+        function_gx = str(function_gx)
+        # init x1, x2
+        x1, x2 = -1, 3
+        rate = 0.0001
+        while True:
+            x = (x1 + x2) / 2
+            y = str(self.derivative_calculator(function_gx))
+            y = eval(y)
+            if y > 0:
+                x2 = x
+            else:
+                x1 = x
+        
+            if abs(y) <= rate:
+                return x
+
+        pass
+
+
+if __name__ == "__main__":
+    a = 1
+    function_fx = 'x*e**(-{}*x)'.format(a)
+    op = Optimizer()
+    '''
+    f(x) = x.e^-ax
+    '''
+    print("f(x) = x.e^-ax")
+    _x = 1/a
+    op.find_extreme(function_fx, a, _x)
+    x_fx = np.linspace(-0.75,5,100)
+    # op.draw(function_fx, x_fx)
+
+    print("*"*100)
+    '''
+    g(x) = f'(x)
+    '''
+    print("g(x) = f'(x)")
+    function_gx = op.derivative_calculator(function_fx)
+    _x = 2/a
+    x_gx = np.linspace(-1.5,5,100)
+    op.find_extreme(function_gx, a, _x)
+    # op.draw(function_gx, x_gx)
+
+    # draw both function
+    op.draw_two(function_fx, function_gx, x_fx)
+
+    value = op.bisection_algorithm(function_gx)
+
+    print(value)
